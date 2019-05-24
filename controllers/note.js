45bf218b245ba8b103e5a26f7b315e58935160d5
ade rahmanp <aderahmanp@gmail.com>
2019-05-24 14:11:30 +0700

    handle error create

--- controllers/note.js
+++ controllers/note.js
@@ -3,6 +3,11 @@ const Note = require("../models/note");
 exports.create = (req, res) => {
   let note = Object.assign(req.body);
   const newnote = { ...note };
+  if (!note.body)
+    return res.status(400).send({
+      message: "Note can not be empty"
+    });
+
   return Note.create(newnote)
     .then(data => {
       res.status(201).json({
